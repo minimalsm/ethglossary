18045797080788707e4568308dd3a08afee5a93b
Joshua <62268199+minimalsm@users.noreply.github.com>
2024-07-05 12:07:02 +0100

    Update layout

--- src/app/fr/[term]/page.js
+++ src/app/fr/[term]/page.js
@@ -14,12 +14,19 @@ export default async function TermPage({ params }) {
 
   return (
     <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">Translations for "{term}"</h1>
       <div className="flex">
         <div className="w-2/3">
+      <h1 className="text-2xl font-bold mb-4">Translations for "{term}"</h1>
+      <h3 className="mb-2 text-lg font-semibold">Examples</h3>
+          <div className="mb-2 p-4 border rounded bg-gray-200">An ethereum transaction requires gas</div>
+          <div className="mb-4 p-4 border rounded bg-gray-200">
+            Gas is the fee required to successfully conduct a transaction or execute a contract on the Ethereum
+            blockchain platform
+          </div>
+      
           <TranslationsList initialTranslations={initialTranslations} termId={termId} />
         </div>
-        <div className="w-1/3">
+        <div className="w-1/3 p-4">
           <CommentsSidebar termId={termId} />
         </div>
       </div>
