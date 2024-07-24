0f806de52ec5897f602bfe78263a954d77c08026
Joshua <62268199+minimalsm@users.noreply.github.com>
2024-07-24 13:42:32 +0100

    Refactor TranslateLink

--- src/components/navigation/TranslateLink.js
+++ src/components/navigation/TranslateLink.js
@@ -1,18 +1,17 @@
-// components/TranslateButton.js
 import { getUserProfile } from '@/lib/userProfile'
 
 export default async function TranslateLink({ userId }) {
   let defaultLanguage = null
 
   if (userId) {
-    const userProfile = await getUserProfile(userId)
-    defaultLanguage = userProfile.default_language
+    const profile = await getUserProfile(userId)
+    defaultLanguage = profile.default_language
   }
 
-  const path = defaultLanguage ? `/${defaultLanguage}/hello` : '/languages'
+  const route = defaultLanguage ? `/${defaultLanguage}/hello` : '/languages'
 
   return (
-    <a href={path} className="btn btn-primary">
+    <a href={route} className="btn btn-primary">
       Translate
     </a>
   )
