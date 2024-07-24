dc6e27bf910b707a30e31eecdd8e60024477b117
Joshua <62268199+minimalsm@users.noreply.github.com>
2024-07-24 13:31:29 +0100

    Remove unused auth

--- src/components/auth/AuthForm.js
+++ src/components/auth/AuthForm.js
@@ -1,24 +1,4 @@
-// components/AuthForm.js
 'use client'
-import { Auth } from '@supabase/auth-ui-react'
-import { ThemeSupa } from '@supabase/auth-ui-shared'
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
-
-// export default function AuthForm() {
-//   const supabase = createClientComponentClient()
-
-//   return (
-
-//     <Auth
-//       supabaseClient={supabase}
-//       appearance={{ theme: ThemeSupa }}
-//       theme="light"
-//       providers={['google']}
-//       onlyThirdPartyProviders
-//       redirectTo={`http://localhost:3000/auth/callback/`}
-//     />
-//   )
-// }
 import { Button } from '@/components/ui/button'
 import { oAuthSignIn } from '../../app/login/actions'
 
