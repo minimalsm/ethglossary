0220b9c7a20f84e2562361770fbcf8b9eb4b1f25
Joshua <62268199+minimalsm@users.noreply.github.com>
2024-07-26 13:39:47 +0100

    Fix leaderboard trigger ui bug

--- src/app/leaderboard/page.js
+++ src/app/leaderboard/page.js
@@ -95,13 +95,13 @@ const UserLeaderboardTable = ({ data }) => {
   )
 }
 
-const UserLeaderboardTab = ({ value, children }) => {
+const UserLeaderboardTabTrigger = ({ value, children }) => {
   console.log('value', value)
   return (
     <TabsTrigger
       // className="text-[13px] bg-white text-black data-[state='active']:bg-[black] data-[state='active']:text-white rounded-full p-0 py-2 px-3"
       // Hardcoding black/white for active state as it was causing a bug where the default styling woudl show
-      className="text-[13px] bg-white text-black data-[state='active']:bg-[#000] data-[state='active']:text-[#fff] rounded-full p-0 py-2 px-3"
+      className=" text-[13px] shadow-none bg-white text-black data-[state=active]:bg-[#000] data-[state=active]:text-[#fff] rounded-full p-0 py-2 px-3"
       value={value}
     >
       {children}
@@ -118,13 +118,19 @@ const UserLeaderboardTabs = ({
 }) => {
   return (
     <Tabs defaultValue="total" className="w-full">
-      <TabsList className="flex gap-2 w-full max-[350px]:h-auto max-[350px]:flex-wrap mb-4 bg-white">
-        <UserLeaderboardTab value="total">Overall</UserLeaderboardTab>
-        <UserLeaderboardTab value="translations">
+      <TabsList className="flex gap-2 w-full max-[350px]:h-auto max-[350px]:flex-wrap mb-4">
+        <UserLeaderboardTabTrigger value="total">
+          Overall
+        </UserLeaderboardTabTrigger>
+        <UserLeaderboardTabTrigger value="translations">
           Translations
-        </UserLeaderboardTab>
-        <UserLeaderboardTab value="comments">Comments</UserLeaderboardTab>
-        <UserLeaderboardTab value="votes">Votes</UserLeaderboardTab>
+        </UserLeaderboardTabTrigger>
+        <UserLeaderboardTabTrigger value="comments">
+          Comments
+        </UserLeaderboardTabTrigger>
+        <UserLeaderboardTabTrigger value="votes">
+          Votes
+        </UserLeaderboardTabTrigger>
       </TabsList>
 
       <TabsContent value="total">
