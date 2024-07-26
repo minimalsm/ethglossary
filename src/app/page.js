407716a1144887636337b0ddf171506af5e34992
Joshua <62268199+minimalsm@users.noreply.github.com>
2024-07-26 16:29:14 +0100

    test env

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 // app/page.js
 import { createSupabaseServerComponentClient } from '@/lib/supabase/server'
+import { getURL } from '../utils/getUrl'
 
 export default async function HomePage() {
   const {
@@ -8,10 +9,12 @@ export default async function HomePage() {
   } = await createSupabaseServerComponentClient().auth.getSession()
 
   const user = session?.user
+  const url = getURL('/auth/login') || 'none'
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <div className="w-full max-w-md text-center">
+        {url}
         {user ? (
           <>
             <h1 className="text-2xl font-bold">Welcome, {user.email}</h1>
