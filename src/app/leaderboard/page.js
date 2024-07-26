a0486535681809cafe3d02232b41c797239a236f
Joshua <62268199+minimalsm@users.noreply.github.com>
2024-07-26 10:17:06 +0100

    Initial leaderboard setup

---
+++ src/app/leaderboard/page.js
@@ -0,0 +1,17 @@
+// app/page.js
+// import { createSupabaseServerComponentClient } from '@/lib/supabase/server'
+
+export default async function LeaderboardPage() {
+  //   const {
+  //     data: { session },
+  //     error,
+  //   } = await createSupabaseServerComponentClient().auth.getSession()
+
+  //   const user = session?.user
+
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Leaderboard</h1>
+    </div>
+  )
+}
