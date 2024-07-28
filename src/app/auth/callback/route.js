44c858592008db99aa5bd3b8cde6f72c230c191f
Joshua <62268199+minimalsm@users.noreply.github.com>
2024-07-28 11:04:15 +0100

    Update route

--- src/app/auth/callback/route.js
+++ src/app/auth/callback/route.js
@@ -9,48 +9,31 @@ export async function GET(request) {
 
   if (code) {
     const supabase = createClient()
-    const { error } = await supabase.auth.exchangeCodeForSession(code)
-    if (!error) {
-      const forwardedHost = request.headers.get('x-forwarded-host') // original origin before load balancer
-      const isLocalEnv = process.env.NODE_ENV === 'development'
-      if (isLocalEnv) {
-        // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
-        return NextResponse.redirect(`${origin}${next}`)
-      } else if (forwardedHost) {
-        return NextResponse.redirect(`https://${forwardedHost}${next}`)
+    const { data, error } = await supabase.auth.exchangeCodeForSession(code)
+
+    // when a user logs in, update their profile in supabase
+    if (!error && data?.user) {
+      const { user } = data
+      const discordUsername = user.user_metadata?.full_name
+
+      const { error: profileError } = await supabase.from('profiles').upsert({
+        id: user.id,
+        user_id: user.id,
+        username: user.email,
+        display_name: discordUsername,
+        avatar_url: user.user_metadata?.avatar_url,
+      })
+
+      if (profileError) {
+        console.error('Error creating/updating profile:', profileError)
       } else {
-        return NextResponse.redirect(`${origin}${next}`)
+        console.log('Profile created/updated successfully')
       }
+
+      return NextResponse.redirect(requestUrl.origin)
+    } else {
+      console.error('Error exchanging code for session:', error)
     }
+    return NextResponse.redirect(`${origin}/auth/error`)
   }
-
-  // if (code) {
-  //   const supabase = createClient()
-  //   const { data, error } = await supabase.auth.exchangeCodeForSession(code)
-
-  //   // when a user logs in, update their profile in supabase
-  //   if (!error && data?.user) {
-  //     const { user } = data
-  //     const discordUsername = user.user_metadata?.full_name
-
-  //     const { error: profileError } = await supabase.from('profiles').upsert({
-  //       id: user.id,
-  //       user_id: user.id,
-  //       username: user.email,
-  //       display_name: discordUsername,
-  //       avatar_url: user.user_metadata?.avatar_url,
-  //     })
-
-  //     if (profileError) {
-  //       console.error('Error creating/updating profile:', profileError)
-  //     } else {
-  //       console.log('Profile created/updated successfully')
-  //     }
-
-  //     return NextResponse.redirect(requestUrl.origin)
-  //   } else {
-  //     console.error('Error exchanging code for session:', error)
-  //   }
-
-  return NextResponse.redirect(`${origin}/auth/error`)
 }
