976771770a4b4fdf4cd1efc5c813d90a7e7418d3
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-07-30 12:59:16 -0700

    chore: update 2025 start/end dates

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,5 +1,4 @@
-// TODO: Add new dates to env vars and update fallback here
-export const START_DATE = process.env.NEXT_PUBLIC_START_DATE || '2024-08-09'
-export const END_DATE = process.env.NEXT_PUBLIC_END_DATE || '2024-08-18'
+export const START_DATE = process.env.NEXT_PUBLIC_START_DATE || '2025-08-25'
+export const END_DATE = process.env.NEXT_PUBLIC_END_DATE || '2025-08-31'
 
 export const YEAR = new Date(START_DATE).getFullYear()
